728697fdbad39a21ea3814487120c45fcd291226
RadCraftplay <radcraftplay2@gmail.com>
2022-12-16 19:07:25 +0100

    Added RegexAdapter

--- adapters/RegexAdapter.py
+++ adapters/RegexAdapter.py
@@ -1,9 +1,20 @@
+import re
 from adapters.Adapter import Adapter
 
-
 class RegexAdapter(Adapter):
-    def can_parse(self, sentence: str):
-        pass
+    def __init__(self, conversation):
+        self.conversation = conversation
+
+    def can_parse(self, sentence: str) -> tuple[bool, float]:
+        for regex in self.conversation.keys():
+            if re.match(regex, sentence) is not None:
+                return True, 1.0
+        return False, 0.0
 
     def get_response(self, sentence: str) -> str:
-        pass
+        for regex, response_func in self.conversation.items():
+            match = re.match(regex, sentence)
+            if match is not None:
+                return response_func(match)
+
+        assert "Trying to process a statement, that can not be processed!"
